736a6273d3574831c43696591fa7c5eee36b92a4
Samar Yadav <samarpratapjnv046@gmail.com>
2025-05-28 15:32:21 +0530

    component-core

---
+++ src/components/Common/ReviewSlider.jsx
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from "react"
+import ReactStars from "react-rating-stars-component"
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react"
+
+// Import Swiper styles
+import "swiper/css"
+import "swiper/css/free-mode"
+import "swiper/css/pagination"
+import "../../App.css"
+// Icons
+import { FaStar } from "react-icons/fa"
+// Import required modules
+import { Autoplay, FreeMode, Pagination } from "swiper"
+
+// Get apiFunction and the endpoint
+import { apiConnector } from "../../services/apiConnector"
+import { ratingsEndpoints } from "../../services/apis"
+
+function ReviewSlider() {
+  const [reviews, setReviews] = useState([])
+  const truncateWords = 15
+
+  useEffect(() => {
+    ;(async () => {
+      const { data } = await apiConnector(
+        "GET",
+        ratingsEndpoints.REVIEWS_DETAILS_API
+      )
+      if (data?.success) {
+        setReviews(data?.data)
+      }
+    })()
+  }, [])
+
+  // console.log(reviews)
+
+  return (
+    <div className="text-white">
+      <div className="my-[40px] h-[184px] max-w-maxContentTab lg:max-w-maxContent">
+      <Swiper
+      slidesPerView={2}
+      spaceBetween={500} // Ensure this is set
+      loop={true}
+      freeMode={true}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      modules={[FreeMode, Pagination, Autoplay]}
+      className="w-full"
+    >
+      {reviews.map((review, i) => {
+        return (
+          <SwiperSlide key={i}>
+            <div className="flex flex-col gap-4 bg-richblack-800 p-4 text-[15px] text-richblack-25 ">
+              <div className="flex items-row gap-2">
+                <img
+                  src={
+                    review?.user?.image
+                      ? review?.user?.image
+                      : `https://api.dicebear.com/5.x/initials/svg?seed=${review?.user?.firstName} ${review?.user?.lastName}`
+                  }
+                  alt=""
+                  className="h-9 w-9 rounded-full object-cover"
+                />
+                <div className="flex flex-col ">
+                  <h1 className="font-semibold text-richblack-5">{`${review?.user?.firstName} ${review?.user?.lastName}`}</h1>
+                  <h2 className="text-[11px] font-medium text-richblack-500">
+                    {review?.course?.courseName}
+                  </h2>
+                </div>
+              </div>
+              <p className="font-medium text-richblack-25 pl-11">
+                {review?.review.split(" ").length > truncateWords
+                  ? `${review?.review
+                      .split(" ")
+                      .slice(0, truncateWords)
+                      .join(" ")} ...`
+                  : `${review?.review}`}
+              </p>
+              <div className="flex items-center gap-2 pl-11">
+                <h3 className="font-semibold text-yellow-100">
+                  {review.rating.toFixed(1)}
+                </h3>
+                <ReactStars
+                  count={5}
+                  value={review.rating}
+                  size={20}
+                  edit={false}
+                  activeColor="#ffd700"
+                  emptyIcon={<FaStar />}
+                  fullIcon={<FaStar />}
+                />
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+      </div>
+    </div>
+  )
+}
+
+export default ReviewSlider
