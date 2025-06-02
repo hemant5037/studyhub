076879e083e8c04cf29806bb13b475f877b33ee7
hemant5037 <hemantkumaryadav542@gmail.com>
2025-06-02 09:27:23 +0530

    mailsender part create

---
+++ server/utils/mailSender.js
@@ -0,0 +1,12 @@
+const nodemailer = require("nodemailer")
+
+const mailSender = async (email, title, body) => {
+  try {
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+      secure: false,
+    })
