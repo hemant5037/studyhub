e9783183cbba9b6859aa0e95338644eca817c7eb
hemant5037 <hemantkumaryadav542@gmail.com>
2025-06-11 11:01:14 +0530

    controllers and database update to new version

--- server/config/database.js
+++ server/config/database.js
@@ -5,14 +5,12 @@ const MONGODB_URL = process.env.MONGODB_URL;
 
 exports.connect = () => {
 	mongoose
-		.connect(MONGODB_URL, {
-			useNewUrlparser: true,
-			useUnifiedTopology: true,
-		})
-		.then(()=>console.log("DB Connected Successfully"))
-		.catch((err) => {
-			console.log("DB Connection Failed");
-			console.log(err);
-			process.exit(1);
-		});
-};
+	  .connect(MONGODB_URL)
+	  .then(() => console.log("DB Connected Successfully"))
+	  .catch((err) => {
+		console.log("DB Connection Failed");
+		console.log(err);
+		process.exit(1);
+	  });
+  };
+  
